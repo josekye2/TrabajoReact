d4769bf633f2d6016fd31fc63790cd54580b9459
Jose Malagon <josekye2@hotmail.com>
2024-09-05 18:58:43 -0500

    First repository}

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Input from './componentes/Input';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Input/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Input.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  const [texto, setTexto] = useState('');
+  const [resultado, setResultado] = useState('');
+
+ 
+  const handleInputChange = (e) => {
+    setTexto(e.target.value);
+  };
+
+  const analizarTexto = () => {
+    const textoNormalizado = texto.replace(/[\W_]/g, '').toLowerCase();
+    const esCapicua = textoNormalizado === textoNormalizado.split('').reverse().join('');
+    setResultado(esCapicua ? 'El texto es capicúa' : 'El texto no es capicúa');
+  };
+
+  return (
+    <div>
+      <h1>Analizador de Capicúa</h1>
+      <input 
+        type="text" 
+        value={texto} 
+        onChange={handleInputChange} 
+        placeholder="Escribe algo"
+      />
+      <button onClick={analizarTexto}>Analizar</button>
+      <p>{resultado}</p>
+    </div>
+  );
+}
+
+export default App;
